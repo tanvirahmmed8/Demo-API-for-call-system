b4e5a9b3639e388a3900dd7cf370251bf73338a5
Tanvir Ahmmed <tanvir.ahmmed@comjagat.org>
2025-04-28 17:08:31 +0600

    error fixed

--- routes/web.php
+++ routes/web.php
@@ -8,10 +8,10 @@ Route::get('/', [UserController::class, 'index']);
 Route::get('/call', [UserController::class, 'call']);
 
 // User routes for web interface
-Route::get('/users', [UserController::class, 'index']);
+Route::get('/users', [UserController::class, 'index'])->name('users.index');
 Route::get('/users/create', [UserController::class, 'create']);
 Route::post('/users', [UserController::class, 'store']);
-Route::get('/users/{id}', [UserController::class, 'show']);
+Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
 Route::get('/users/{id}/edit', [UserController::class, 'edit']);
 Route::put('/users/{id}', [UserController::class, 'update']);
 
@@ -19,6 +19,6 @@ Route::put('/users/{id}', [UserController::class, 'update']);
 Route::get('/tickets', [TicketController::class, 'index']);
 Route::get('/tickets/create', [TicketController::class, 'create']);
 Route::post('/tickets', [TicketController::class, 'store']);
-Route::get('/tickets/{id}', [TicketController::class, 'show']);
+Route::get('/tickets/{id}', [TicketController::class, 'show'])->name('tickets.show');
 Route::get('/tickets/{id}/edit', [TicketController::class, 'edit']);
 Route::put('/tickets/{id}', [TicketController::class, 'update']);
